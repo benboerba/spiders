202f610083c97613e3872a2eac0350cc6fe194fa
benboerba <1154625636@qq.com>
2019-08-25 13:35:44 +0800

    add mkdir

--- spiders.go
+++ spiders.go
@@ -11,90 +11,77 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
-
-	"gopkg.in/mgo.v2/bson"
 )
 
 const (
 	formula    string = `https://[^"{]+\.jpg`
-	cookie     string = `_zap=8f3061cf-1d28-4f66-ac6e-5bc9b8ec2fc3; _xsrf=C10wcdvVeQa9tEd1DLJHxzsgWZiuMzGM; d_c0="AEDiBEcrvQ-PTjkYe2trS7dcTNmL2x0Ky0w=|1563172957"; capsion_ticket="2|1:0|10:1563172970|14:capsion_ticket|44:ZTQ0ZjQ5YzkxNmJiNGNhZWJjNzExMGYxNTdmY2QzNzI=|c9f4c633ec22ae7328495bb76860f53a012b700e73fc22e0540709c374703b0b"; z_c0="2|1:0|10:1563173003|4:z_c0|92:Mi4xQjY1bUJBQUFBQUFBUU9JRVJ5dTlEeVlBQUFCZ0FsVk5pMjRaWGdBQzNwSnRMSVFMM29OUXl1T1UwOHRxSHl1aUN3|3dda9b7b871a7c8c0edfc159f5e50a1017102c2b02cc5aadafa1229a43beaa4b"; tst=r; q_c1=095bc3f3f18a429baec8651cb3d78b30|1563173071000|1563173071000; tgw_l7_route=060f637cd101836814f6c53316f73463`
+	cookie     string = `_xsrf=pCztpO8EN6FSWNC7ih4f444fxSVvkITd; _zap=ef68d577-531e-4c5d-b38e-ddb7eaf49dc1; d_c0="AKAj7XI2zg-PTuQsnObF_rvWLm3o2AyNtqY=|1564316737"; capsion_ticket="2|1:0|10:1564316750|14:capsion_ticket|44:NGRlZjRjNzNlNDliNDRlMGJlYjg2Y2UxMmQ4MWY4NDk=|f64af661b96d24449ec403d7ef22745564a92c1be9fea7fdd05818ccbf154344"; z_c0="2|1:0|10:1564316784|4:z_c0|92:Mi4xQjY1bUJBQUFBQUFBb0NQdGNqYk9EeVlBQUFCZ0FsVk5jT0lxWGdCVGRQMWxDRXRZMlNPWjJwMDd2XzhYS1F5UnNB|3896b5f1763304a98c2001ae5d449cefb84d5d953db0e5b3a43779ddabf8075c"; tst=r; q_c1=2619259abaad49f897c5d4e7f4e5fb26|1564326458000|1564326458000; tgw_l7_route=f2979fdd289e2265b2f12e4f4a478330`
 	prefix     string = "https://www.zhihu.com/api/v4/questions/"
 	postfix    string = "/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=20&offset="
 	ppos       string = "&sort_by=default"
-	myfilepath string = `E:\Resources\photo\`
+	myfilepath string = `C:\Users\Administrator\Desktop\picture100\`
 	//keyword    string = `_hd`
 )
 
 func main() {
-	if err := InitMgo(); err != nil {
-		fmt.Println("init mgo ", err)
+	t := time.Now()
+	//name := "zhihuurl.txt"
+	offset := 0
+	questionid := "318927654"
+	content := geturlRespHtml(prefix + questionid + postfix + strconv.Itoa(offset) + ppos)
+	tmpindex := strings.LastIndex(content, "totals") //totals出现的下标
+	//total处理      开始
+	tmp := []byte{}
+	tmpindex += 8
+	for content[tmpindex] >= 48 && content[tmpindex] <= 57 {
+		tmp = append(tmp, content[tmpindex])
+		tmpindex++
+	}
+	total, err := strconv.Atoi(string(tmp))
+	if err != nil {
 		return
 	}
+	//total处理      结束
 
-	fmt.Println("init finish")
-
-	sess := GetMgoS()
-	defer sess.Close()
-
-	c := GetMgoC(sess, "medex", "student")
-
-	var data interface{}
-	err := c.Find(bson.M{"name": "hhh"}).One(&data)
+	//创建文件夹
+	err = os.Mkdir(myfilepath, os.ModePerm)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("mkdir failed![%v]\n", err)
+	} else {
+		fmt.Printf("mkdir success!\n")
 	}
-	fmt.Println("GetDataViaName() student ", data)
 
-	// t := time.Now()
-	// //name := "zhihuurl.txt"
-	// offset := 0
-	// questionid := "26620889"
-	// content := geturlRespHtml(prefix + questionid + postfix + strconv.Itoa(offset) + ppos)
-	// tmpindex := strings.LastIndex(content, "totals") //totals出现的下标
-	// //total处理      开始
-	// tmp := []byte{}
-	// tmpindex += 8
-	// for content[tmpindex] >= 48 && content[tmpindex] <= 57 {
-	// 	tmp = append(tmp, content[tmpindex])
-	// 	tmpindex++
-	// }
-	// total, err := strconv.Atoi(string(tmp))
-	// if err != nil {
-	// 	return
-	// }
-	// //total处理      结束
+	pagenum := total/20 + 1
+	ch := make(chan int, pagenum)
 
-	// pagenum := total/20 + 1
-	// ch := make(chan int, pagenum)
+	fmt.Println(total)
 
-	// fmt.Println(total)
+	fmt.Println("pagenum : ", pagenum)
+	WriteWithIoutil("1.txt", content)
 
-	// fmt.Println("pagenum : ", pagenum)
-	// WriteWithIoutil("1.txt", content)
+	for offset < total {
+		go downloadgoroutine(prefix+questionid+postfix+strconv.Itoa(offset)+ppos, ch)
 
-	// for offset < total {
-	// 	go downloadgoroutine(prefix+questionid+postfix+strconv.Itoa(offset)+ppos, ch)
+		// content = geturlRespHtml(prefix + questionid + postfix + strconv.Itoa(offset) + ppos)
+		// str += analyze(content)
+		offset += 20
+		fmt.Println("offset : ", offset)
+	}
 
-	// 	// content = geturlRespHtml(prefix + questionid + postfix + strconv.Itoa(offset) + ppos)
-	// 	// str += analyze(content)
-	// 	offset += 20
-	// 	fmt.Println("offset : ", offset)
-	// }
+	for j := 0; j < pagenum; j++ {
+		tmp := <-ch
+		fmt.Println(tmp)
+	}
 
-	// for j := 0; j < pagenum; j++ {
-	// 	tmp := <-ch
-	// 	fmt.Println(tmp)
-	// }
-
-	// elapsed := time.Since(t)
-	// fmt.Println("program elapsed:", elapsed)
+	elapsed := time.Since(t)
+	fmt.Println("program elapsed:", elapsed)
 }
 
 func downloadgoroutine(url string, ch chan int) {
